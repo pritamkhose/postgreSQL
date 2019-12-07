470eb6fe2fc37cae7d5fa9cd413f2f978bcd301c
Pritam Khose <pritamkhose@gmail.com>
2019-12-08 00:34:52 +0530

    Update JpaConfig.java

--- src/main/java/com/pritam/postgreSQL/config/JpaConfig.java
+++ src/main/java/com/pritam/postgreSQL/config/JpaConfig.java
@@ -27,9 +27,19 @@ public class JpaConfig {
 		
 		// Run in Heroku production Java
 		// https://devcenter.heroku.com/articles/config-vars
-		dataSourceBuilder.url(System.getenv("DBurl"));
-		dataSourceBuilder.username(System.getenv("DBusername"));
-		dataSourceBuilder.password(System.getenv("DBpassword"));
+// 		dataSourceBuilder.url(System.getenv("DBurl"));
+// 		dataSourceBuilder.username(System.getenv("DBusername"));
+// 		dataSourceBuilder.password(System.getenv("DBpassword"));
+		
+		// Run in Heroku production Java where DATABASE_URL change dyanamically by Heroku postgreSQL
+		String DATABASE_URL = System.getenv("DATABASE_URL");
+		String [] DbArr = DATABASE_URL.split("/", -1);
+		String DBurl = "jdbc:postgresql://" + DATABASE_URL.split("@", -1)[1];
+		String DBusername = DbArr[2].split(":", -1)[0]; 
+		String DBpassword = DbArr[2].split(":", -1)[1].split("@", -1)[0];
+		dataSourceBuilder.url(DBurl);
+		dataSourceBuilder.username(DBusername);
+		dataSourceBuilder.password(DBpassword);
 
 		// Run in CMD Java
 // 		dataSourceBuilder.url(System.getProperty("DBurl"));
